81a18a44eb0045d9195b362363739304639114b2
Adam29C <adamcatholic29@gmail.com>
2025-04-28 17:15:26 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Testing from "./testing";
 import "./App.css";
+import testing from "./testing";
 
 const RenderPage = () => {
   return (
@@ -11,6 +13,7 @@ const RenderPage = () => {
       <Router>
         <Routes>
           <Route path="/main" element={<App />} />
+          <Route path="/" element={<Testing />} />
         </Routes>
       </Router>
     </>
