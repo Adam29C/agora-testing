44744591d27fcb5246e1d9aed843fe7326d4582b
Adam29C <adamcatholic29@gmail.com>
2025-04-28 17:27:50 +0530

    update build

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Testing from "./testing";
 import "./App.css";
-import testing from "./testing";
 
 const RenderPage = () => {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import fs from 'fs'
+import { defineConfig } from "vite";
+import fs from "fs";
 
 export default defineConfig({
   server: {
-    // https: {
-    //   key: fs.readFileSync('key.pem'),
-    //   cert: fs.readFileSync('cert.pem'),
-    // },
-    // host: true, // allows access from mobile via LAN IP
     port: 5173,
   },
-})
+});
