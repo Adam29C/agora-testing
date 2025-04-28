750181f41f4da073e3af78e9ba5bacb4d16faa9b
Adam29C <adamcatholic29@gmail.com>
2025-04-28 17:10:25 +0530

    update build

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import fs from 'fs'
 
 export default defineConfig({
   server: {
-    https: {
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem'),
-    },
-    host: true, // allows access from mobile via LAN IP
+    // https: {
+    //   key: fs.readFileSync('key.pem'),
+    //   cert: fs.readFileSync('cert.pem'),
+    // },
+    // host: true, // allows access from mobile via LAN IP
     port: 5173,
   },
 })
