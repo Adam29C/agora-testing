b6b54df06d9127060509671d393cab994b591bdf
Adam29C <adamcatholic29@gmail.com>
2025-04-28 18:41:59 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import fs from "fs";
-
+import { defineConfig } from 'vite'
+import fs from 'fs'
 export default defineConfig({
-  server: {
-    port: 5173,
-  },
-});
+server: {
+host: '0.0.0.0', // All interfaces
+port: 5173,
+allowedHosts: [
+'chatit.minidog.club', // Add your domain here
+],
+},
+})
